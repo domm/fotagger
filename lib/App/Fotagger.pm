b1bdcbc9f32d30d1f6d3843dd4b7c00271c3bb11
Thomas Klausner <domm@cpan.org>
2009-03-14 22:45:45 +0100

    initial commit

---
+++ lib/App/Fotagger.pm
@@ -0,0 +1,34 @@
+package App::Fotagger;
+
+use strict;
+use warnings;
+use 5.010;
+use version; our $VERSION = version->new('0.01');
+
+1;
+__END__
+
+=head1 NAME
+
+App::Fotagger -
+
+=head1 SYNOPSIS
+
+  use App::Fotagger;
+
+=head1 DESCRIPTION
+
+App::Fotagger is
+
+=head1 AUTHOR
+
+Thomas Klausner E<lt>domm {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
